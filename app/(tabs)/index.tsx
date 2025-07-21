c0d851ac40e4db37d458999c44bd0202c47e260f
mbartos00 <maciekbartos8@gmail.com>
2025-07-21 14:43:24 +0200

    feat: swap momentjs to spacetime

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,61 +1,49 @@
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import Calendar from '@/components/calendar/Calendar';
-import moment from 'moment';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import Calendar from "@/components/calendar/Calendar";
+import spacetime from "spacetime";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/pagaspot-screen.png')}
+          source={require("@/assets/images/pagaspot-screen.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Hej!</ThemedText>
         <HelloWave />
       </ThemedView>
 
-
       <Calendar
-        from={moment("2025-08-01")}
-        offerDays={['2025-08-10','2025-08-11','2025-08-12','2025-08-15','2025-08-17','2025-08-18']}
+        from={spacetime("2025-08-01")}
+        offerDays={[
+          "2025-08-10",
+          "2025-08-11",
+          "2025-08-12",
+          "2025-08-15",
+          "2025-08-17",
+          "2025-08-18",
+        ]}
         orderDays={[]}
       />
-
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Twoje zadanie</ThemedText>
-        <ThemedText>
-          Usprawnij kalendarz powyżej według instrukcji z maila.
-        </ThemedText>
-        <ThemedText>
-          W components/calendar/Calendar.tsx znajdziesz kod które trzeba rozwinąć.
-        </ThemedText>
-        
-        <ThemedText>
-          Zwróć uwagę na TODO w komentarzach, zrób tyle ile uważasz.
-          Poświęć na to zadanie max 1.5 godziny
-        </ThemedText>
-        <ThemedText>
-          Ten tekst możesz usunąć
-        </ThemedText>
-      </ThemedView>
-
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -63,10 +51,10 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
